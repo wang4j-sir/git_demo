41670fb2177efae37a4824b79c15694437ffe93f
zhangsan <a965752@126.com>
2023-02-25 15:32:16 +0800

    mac commit

--- src/main/java/com/example/git_demo/controller/TestSpringBootController.java
+++ src/main/java/com/example/git_demo/controller/TestSpringBootController.java
@@ -17,6 +17,7 @@ public class TestSpringBootController {
     public static void main(String[] args) {
         System.out.println("111111");
         System.out.println("22222222");
+        System.out.println("mac");
     }
 
 
